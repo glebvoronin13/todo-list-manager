655217e2ea7b8a67aed779387efc542d52c7c622
glib.voronin <glib.voronin@accenture.com>
2017-10-05 20:50:40 +0300

    Add user id

--- server/src/controllers/todos.ts
+++ server/src/controllers/todos.ts
@@ -1,9 +1,13 @@
 import Todo from '../models/Todo';
-import { sendResponse } from '../helpers';
+import { sendResponse, sendErrorResponse } from '../helpers';
 
 const getTodos = (req, res) => {
   console.log('user', req.user);
-  Todo.find({ }, function (err, data) {
+  if (!req.user) {
+    return sendErrorResponse(res, 401, 'Not Authorised');
+  }
+  const id = req.user._id;
+  Todo.find({ uid: id }, function (err, data) {
         if (err) {
             return console.error(err);
         }
